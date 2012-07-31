eeb34d5705fb4411fb5298c5f1e393d5f090ee12
Guillaume Pernelle <gpernelle@gmail.com>
2012-07-31 13:15:47 -0400

    needle segmentation

--- Wizard/iGyneNeedleSegmentationStep.py
+++ Wizard/iGyneNeedleSegmentationStep.py
@@ -26,7 +26,60 @@ class iGyneNeedleSegmentationStep( iGyneStep ) :
     '''
 
     self.__layout = self.__parent.createUserInterface()
-
+    
+    
+    needleLabel = qt.QLabel( 'Needle Label:' )
+    self.__needleLabelSelector = slicer.qMRMLNodeComboBox()
+    self.__needleLabelSelector.toolTip = "Choose the needle-label image"
+    self.__needleLabelSelector.nodeTypes = ['vtkMRMLScalarVolumeNode']
+    self.__needleLabelSelector.setMRMLScene(slicer.mrmlScene)
+    self.__needleLabelSelector.addEnabled = 1
+    self.__layout.addRow( needleLabel, self.__needleLabelSelector )
+    # followupScanLabel = qt.QLabel( 'Followup scan:' )
+    # self.__followupVolumeSelector = slicer.qMRMLNodeComboBox()
+    # self.__followupVolumeSelector.toolTip = "Choose the followup scan"
+    # self.__followupVolumeSelector.nodeTypes = ['vtkMRMLScalarVolumeNode']
+    # self.__followupVolumeSelector.setMRMLScene(slicer.mrmlScene)
+    # self.__followupVolumeSelector.addEnabled = 0
+	
+	#Load Template Button 
+    self.needleButton = qt.QPushButton('Segment Needles')
+    self.__layout.addRow(self.needleButton)
+    self.needleButton.connect('clicked()', self.needleSegmentation)
+    
+    self.updateWidgetFromParameters(self.parameterNode())
+    
+  def needleSegmentation(self):
+    scene = slicer.mrmlScene
+    pNode = self.parameterNode()
+    inputImage = Helper.getNodeByID(pNode.GetParameter("baselineVolumeID"))
+    needleLabelID = pNode.GetParameter('needleLabelID')
+    needleLabelNode = None
+    
+    if needleLabelID != '':
+      needleLabelNode = Helper.getNodeByID(needleLabelID)
+    else:
+      needleLabelNode = slicer.mrmlScene.CreateNodeByClass('vtkMRMLScalarVolumeNode')
+      slicer.mrmlScene.AddNode(needleLabelNode)
+      pNode.SetParameter('needleLabelID', needleLabelNode.GetID())
+      
+    needleVolumeID = pNode.GetParameter('needleVolumeID')
+    needleVolumeNode = None
+    
+    if needleVolumeID != '':
+      needleVolumeNode = Helper.getNodeByID(needleVolumeID)
+    else:
+      needleVolumeNode = slicer.mrmlScene.CreateNodeByClass('vtkMRMLModelNode')
+      slicer.mrmlScene.AddNode(needleVolumeNode)
+      pNode.SetParameter('needleVolumeID', needleVolumeNode.GetID())
+   
+    parameters = {} 
+    parameters['InputImageName'] = needleVolumeNode.getID()
+    parameters["InputLabelImageName"] = needleLabelNode.GetID() 
+    parameters['outputVtkName'] = needleVolumeNode
+    module = slicer.modules.mainlabelneedletrackingcli 
+    self.__cliNode = None
+    self.__cliNode = slicer.cli.run(module, self.__cliNode, parameters)
     
   def validate( self, desiredBranchId ):
     '''
